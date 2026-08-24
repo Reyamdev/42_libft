48e2487e12f8a493dde1e66ef44505efddc5b8b8
rimayer <reyam6252@gmail.com>
2026-08-24 23:14:13 +0200

    strlcat  bug fixes

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,7 +17,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 	size_t	src_len;
 	size_t	i;
-	
+
 	dst_len = 0;
 	while (dst_len < dstsize && dst[dst_len])
 		dst_len++;
@@ -27,9 +27,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	i = 0;
 	while (src[i] && dst_len + i + 1 < dstsize)
 	{
-		dst[dst_len + i]
+		dst[dst_len + i] = src[i];
 		i++;
 	}
-	dst[dest_len + i] = '\0';
+	dst[dst_len + i] = '\0';
 	return (dst_len + src_len);
 }
