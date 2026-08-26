62f595d48c0790ae2e120f2d5beb4e8df2c6adc9
rimayer <reyam6252@gmail.com>
2026-08-26 12:33:34 +0200

    memmove fix for norminette

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,27 +6,20 @@
 /*   By: reyam <reyam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 19:15:55 by reyam             #+#    #+#             */
-/*   Updated: 2026/08/25 16:07:56 by reyam            ###   ########.fr       */
+/*   Updated: 2026/08/26 11:52:34 by reyam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// The memmove() function copies len bytes from string src to string dst.
-// The two strings may overlap; the copy is always done in a non-destructive manner.
-
-// The safeguard for that is entirely about choosing a copy direction that won’t destroy source bytes before they’ve been read.
-// to achieve that,	d < s compares the addresses stored in the two pointers, not the bytes they point to.
-// like is address 0x1004 before address 0x1000?
-// if dest is before source then
-
-// to sum up:
-// copy forward if dest is before src; otherwise copy backward.
-// this prevents overlapping memory from overwriting bytes before they are copied.
+/*
+** Copies len bytes from src to dest.
+** Handles overlapping memory safely by choosing the right copy direction.
+*/
 
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	unsigned char	*d;
+	unsigned char		*d;
 	const unsigned char	*s;
 
 	d = (unsigned char *)dest;
@@ -35,9 +28,7 @@ void *ft_memmove(void *dest, const void *src, size_t len)
 	{
 		while (len > 0)
 		{
-			*d = *s;
-			d++;
-			s++;
+			*d++ = *s++;
 			len--;
 		}
 	}
@@ -47,9 +38,7 @@ void *ft_memmove(void *dest, const void *src, size_t len)
 		s += len;
 		while (len > 0)
 		{
-			d--;
-			s--;
-			*d = *s;
+			*--d = *--s;
 			len--;
 		}
 	}
